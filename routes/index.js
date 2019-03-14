e636733481785c0d43ed9da43093cc66cf886dd8
Billy Infante <infbil15@gmail.com>
2019-03-14 13:47:05 +0100

    cv template

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,25 @@
 var express = require('express');
 var router = express.Router();
 
-const dataRouter = require('../data/data.json');
+const data = require('../data/data.json');
 
 /* GET HOME PAGE. */
 router.get('/', function(req, res, next) {
-
-  res.render('index', { title : 'CV', data : dataRouter });
-  console.log(dataRouter);
+  res.render('index', { title : 'Hello World!', data : data });
 });
+
+/* GET HOME PAGE. */
+router.get('/portfolio', function(req, res, next) {
+
+  res.render('portfolio', { data : data });
+});
+
+/* GET HOME PAGE. */
+router.get('/about', function(req, res, next) {
+  
+  res.render('about', { data : data });
+});
+
 
 /* JSON DATA. */
 router.get('/data', function(req, res, next) {
