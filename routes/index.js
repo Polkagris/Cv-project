37503bc912414bc27a3751a640a17ea912fbf8ac
Your Name <you@example.com>
2019-03-14 10:55:25 +0100

    json file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const dataRouter = require('../data/data.json');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'OK' });
 });
+
+/* GET home page. */
+router.get('/data', function(req, res, next) {
+  res.json(dataRouter);
+});
+
 
 module.exports = router;
